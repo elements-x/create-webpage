a51ed8b09569d58ae5c0fbe8592b4951f3419bed
Allen Kim <allenhwkim@gmail.com>
2021-08-21 13:18:54 -0400

    updated

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const fs = require('fs-extra');
 const mustache = require('mustache');
 const fetch = require('node-fetch');
+const showdown = require('showdown');
 
 async function getPackageInfo(user, repoName) {
   if (user && repoName) { // get it from github api
@@ -16,7 +17,6 @@ async function getPackageInfo(user, repoName) {
   } else { // get it from package.json
     const pjson = require('./package.json');
     pjson.html_url = (pjson.repository || {}).url;
-    pjson.issue_events_url = (pjson.bugs || {}).url;
     return pjson;
   }
 }
@@ -27,7 +27,8 @@ function getAnswers(pkgInfo) {
   pkgInfo.name = pkgInfo.name || 'unnamed';
   pkgInfo.description = pkgInfo.name || 'Description';
   pkgInfo.html_url = pkgInfo.html_url || `https://github.com/${pkgInfo.name}`;
-  pkgInfo.issue_events_url = pkgInfo.issue_events_url || `https://github.com/${pkgInfo.name}/issues`;
+  pkgInfo.issue_events_url = `${pkgInfo.html_url}/issues`;
+  pkgInfo.license = pkgInfo.license.name || pkgInfo.license || 'MIT';
 
   const questions = [
     {type: 'input', name: 'name', message: 'Project Name', default: pkgInfo.name, validate: required},
@@ -36,7 +37,7 @@ function getAnswers(pkgInfo) {
     {type: 'input', name: 'version', message: 'Version', default: pkgInfo.version, validate: required},
     {type: 'input', name: 'repo_url', message: 'Repo. URL', default: pkgInfo.html_url, validate: required},
     {type: 'input', name: 'issue_url', message: 'Issue URL', default: pkgInfo.issue_events_url},
-    {type: 'license', name: 'license', message: 'License', default: pkgInfo.license || 'MIT'},
+    {type: 'license', name: 'license', message: 'License', default: pkgInfo.license},
   ];
 
   return inquirer.prompt(questions);
@@ -68,6 +69,26 @@ function copyDir(fromDir, toDir, replacements) {
   });
 }
 
+async function buildHomeHtml(outDir, user, repoName) {
+  const destPath = path.join(outDir, 'pages', 'home.html');
+  let readmeContents;
+  
+  if (user && repoName) {
+    const url = `https://raw.githubusercontent.com/${user}/${repoName}/main/README.md`;
+    console.log(`* Generating ${destPath} from ${url}`);
+    const resp = await fetch(url);
+    readmeContents = await resp.text();
+  } else {
+    console.log(`* Generating ${destPath} from ./README.md`);
+    readmeContents = fs.readFileSync('./README.md', 'utf8');
+  }
+  const options = {ghCompatibleHeaderId: true, simpleLineBreaks: true, ghMentions: true, tables: true};
+  const converter = new showdown.Converter(options)
+  converter.setFlavor('github');
+  const html = converter.makeHtml(readmeContents);
+  fs.outputFileSync(destPath, '<div class="markdown-body">' + html + '</div>'); 
+}
+
 async function run() {
   const [user, repoName] = (process.argv[2]||'').split('/').slice(-2);
   const pkgInfo = await getPackageInfo(user, repoName);
@@ -77,7 +98,8 @@ async function run() {
   console.log(`* Processing answers ${JSON.stringify(answers, null, '  ')}`);
 
   copyDir(path.join(__dirname, 'template'), answers.outDir, answers);
-  console.log(`Done. To open pages run "cd ${answers.outDir} && npx http-server -o ${answers.outDir}"`);
+  await buildHomeHtml(answers.outDir, user, repoName);
+  console.log(`Done. To open pages run "cd ${answers.outDir} && npx http-server -o"`);
 }
 
 run();
