ae4acb1044e3cf2e2e19b6d97adb6b45ec95e1d5
Allen Kim <allenhwkim@gmail.com>
2021-08-20 23:26:26 -0400

    updated

--- docs/assets/script.js
+++ docs/assets/script.js
@@ -1,20 +1,8 @@
 // use `$` instead of `document.querySelector`
 window.$ = document.querySelector.bind(document);
 window.addEventListener('DOMContentLoaded', function() {
-  // enable/disable outline for click and tab
-  document.body.addEventListener('click', 
-  e => document.body.classList.remove('a11y-outline') );
-  document.body.addEventListener('keydown', 
-  e => (e.key === 'Tab') && document.body.classList.add('a11y-outline') );
-  $('#dark-mode-switch').addEventListener('click', event => {
-    const theme = $('#dark-mode-switch').getAttribute('aria-checked') === 'true' ? 'light' : 'dark';
-    document.documentElement.setAttribute('data-theme', theme);
-    localStorage.setItem('theme', theme);
-  });
-  
-  const theme = localStorage.getItem('theme');
-  document.documentElement.setAttribute('data-theme', theme);
-  $('#dark-mode-switch').setAttribute('status', theme === 'dark' ? 'on' : 'off');
+  $('#'
+  id="dark-mode"
 });
 
 // Single Page Apps for GitHub Pages
